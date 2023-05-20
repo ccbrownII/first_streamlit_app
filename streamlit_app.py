156a510418de005a1ee2f7e552438c8823838e03
ccbrownII <118418206+ccbrownII@users.noreply.github.com>
2023-05-20 11:16:57 -0500

    Update streamlit_app.py
    Set index list to read fruit instead of list numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Lets add a pick list so customers can choose what fruit they want
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
