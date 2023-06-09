391a63cbc40327885b00b0a68f86014fe86305b1
ccbrownII <118418206+ccbrownII@users.noreply.github.com>
2023-06-09 17:44:24 -0500

    Update streamlit_app.py
    Added buttons for menu cleanliness and user interaction. Lists will not show until user engages button

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,18 +45,30 @@ except URLError as e:
   streamlit.error
 
 #line below will tell your py file to use the library you added to the project
-
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+         return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 #allow end user to add fruit to list
-add_my_fruit = streamlit.text_input('What fruit would you like to see added?', '')
-streamlit.write('Thanks for suggesting', add_my_fruit)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my cur:
+        my_cur.execute ("insert into fruit_load_list values ('from streamlit')");
+        return "Thanks for adding " + new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
-#update table with fruit end user chose to add
-my_cur.execute ("insert into fruit_load_list values ('from streamlit')");
+
